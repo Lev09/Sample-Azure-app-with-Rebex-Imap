620155642a8480debdefa53645b579442d3437f4
Levon Hakobyan <levon.hakopyan@gmail.com>
2022-06-30 20:53:42 +0400

    fix project name

---
+++ mailAppMS_Graph/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Identity.Client;
+
+namespace mailAppMS_Graph
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static string ClientId = "3e12a2ab-d4a2-400f-82c7-9157a095dd93";
+
+        private static string Tenant = "common";
+
+        private static IPublicClientApplication _clientApp;
+
+        public static IPublicClientApplication PublicClientApp { get { return _clientApp; } }
+
+        static App()
+        {
+            _clientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, Tenant)
+                .WithDefaultRedirectUri()
+                .Build();
+        }
+    }
+}
